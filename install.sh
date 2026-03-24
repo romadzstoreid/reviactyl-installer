50ad1bac87cd979b1cfb89a4bcb0620d3728d065
romadzstoreid <romadzstoreid@gmail.com>
2026-03-24 22:37:33 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-export SCRIPT_RELEASE="v1.1.0"
+export SCRIPT_RELEASE="v1.2"
 LOG_PATH="/var/log/sapurahost-installer.log"
 
 SOFT_BLUE='\e[38;5;111m'
@@ -17,76 +17,73 @@ if ! [ -x "$(command -v curl)" ]; then
 fi
 
 output() {
-    echo -e "${SOFT_BLUE}❖${NC} ${WHITE}$1${NC}"
+    echo -e "${SOFT_BLUE}✦${NC} ${WHITE}$1${NC}"
 }
 
 error() {
-    echo -e "${RED}❖ ERROR:${NC} ${WHITE}$1${NC}"
+    echo -e "${RED}✦ ERROR:${NC} ${WHITE}$1${NC}"
 }
 
 welcome() {
+    local width=60
+    local title="SAPURAHOST - THEME AUTO INSTALLER"
+    local release="Script Release: ${SCRIPT_RELEASE}"
+
+    local pad_title=$(( (width - ${#title}) / 2 ))
+    local pad_rel=$(( (width - ${#release}) / 2 ))
+
     clear
-    # Definisi Warna
-    local BLUE='\033[38;5;117m' # Soft Blue
-    local WHITE='\033[1;37m'
-    local NC='\033[0m'
-
-    echo -e "${BLUE}"
-    cat << "EOF"
- .;...                                               Script Installer - Thema Otomatis
-         .['_"..           ..'`..                    ----------
-         .['1-,!....   ....';,!..                    
-   .}'{:I)i!+^..'_l:-!~]::'    .....                 • Telegram: @rmddz
-         .+'_I.`^>-^--'+"^!;iI'.   ..I,i!';'.`";!,.. • Website: romadzrbg.my.id
-         .i;>".....'+....I1i~".
-  ...~};[-!~+>!^...    ...                       
-      ...<]l_+;.. ...  ..!j~`..     ..]tI|1<]}+<^.
- ..'.                       
-   ..`^,:`:'...       .':"I"..     .^,,,,,":,":"::`,.  ....   .',.                
- .`I^!,:".'..         ..."!,I".
- 'I;,I::I:"lI,!:Ii^!!^I..  ..>.'!'                
- ...,_Ii+,-<,_l`.  ..'.''.II~-:]"..  .....I_<l;.....     ...'`-:]'                
-        .``'({l-.'`.|/`t_i)i-{;{[:''.
- ^{]''..      ...`]I/x`r).                
-           .`~:>.>]"]]`]i;_Ii~,~<:<!`..  `~>..      ...:Ii}:]1`}_.......          
-            ,>;<l!-,<~.........."<i`"`...............",+Il<"~!^>I"`'``..          
-            "!l-i>_"!.
- .'`'.   ^i!I]!~}"{[,{<!<`,:'}+:[li<^.i^+!I!`....          
-            .`<;,"..  ...;!..    .^>II~,>_^<!:i:I!"ll,I:`...+`}+:<'.....          
-            .^<^..    .`,]>.     ..;l!],_!`+i;<;!>"ii,^'.
- .;'|[;-`...;.          
-            .`;.......l~;-!,^..   ...,+>l[i........,'.     ..>|!_[i<,...          
-            .'lII>">!`~l:i;I:'....   ..:`-+^>..            ..!i^{_!{l+>'!I.....   
-             .`,,I^II'!:";,:;",;"''.....'i!`!:'...            ..`',>,!<`>i"!:''''.
-               .")i/c'nf!n--r;|j"):^^...':),(+i{:.            .......:|"f|I/_~|!}"
-       
-    ...  .>t!|n.x/!r+_f:,:...    .'[{,)~.^'.       .......:-!t>{f^/)I["^`...
-         .i....'`;>"l~'>!,<;I`.      ..`'<>^>:..         .','.^i^>I:<"!i.I...     
-             .'l!i}".'.,_;{i~".
- ...'+,1["+..........    ..'+],}<!;.'.          
-             .....      .I}>^..  ..`<)!]|``1It+_/;||`|..   ..(-:-`l>`.            
-                        ..'...  ..,il_;<-`[+:[!i-,--`[>''....->">`....            
-              
-                   .`,"^,^,^,"'^^'"`....,`,"^,^`^`:^`^.                
-                      ..'^.     .,l.......        ..:!!+>!+;<{`>                  
-               
-        .....                          .`!i!+:!_..                  
-                                                  
-    .`ili-".                     
-                                                     ....;+,.                     
-    
-                                                      .l^.
-EOF
-    echo -e "${NC}"
+    echo -e "${SOFT_BLUE}╔════════════════════════════════════════════════════════════╗${NC}"
+    echo -e "${SOFT_BLUE}║$(printf '%*s' $width "")║${NC}"   
+    echo -ne "${SOFT_BLUE}║${WHITE}"
+    printf "%*s%s%*s" $pad_title "" "$title" $((width - pad_title - ${#title})) ""
+    echo -e "${SOFT_BLUE}║${NC}"    
+    echo -ne "${SOFT_BLUE}║${SOFT_BLUE}"
+    printf "%*s%s%*s" $pad_rel "" "$release" $((width - pad_rel - ${#release})) ""
+    echo -e "${SOFT_BLUE}║${NC}"    
+    echo -e "${SOFT_BLUE}║$(printf '%*s' $width "")║${NC}"
+    echo -e "${SOFT_BLUE}╚════════════════════════════════════════════════════════════╝${NC}"
+    echo ""
 }
-
-
 
 execute_action() {
     local action=$1
     echo -e "\n\n* SapuraHost-Installer $(date) - Action: $action \n\n" >>$LOG_PATH
 
     case $action in
+        "reset_password")
+            output "Menghasilkan Password Random Baru..."
+            NEW_PASS=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16)
+            
+            output "Menerapkan password baru ke user root..."
+            echo "root:$NEW_PASS" | chpasswd
+            
+            local width=60
+            local line1="DATA LOGIN VPS BARU (ROOT)"
+            local line2="Username : root"
+            local line3="Password : $NEW_PASS"
+
+            local pad1=$(( (width - ${#line1}) / 2 ))
+            local pad2=$(( (width - ${#line2}) / 2 ))
+            local pad3=$(( (width - ${#line3}) / 2 ))
+
+            echo -e "\n${GREEN}╔════════════════════════════════════════════════════════════╗${NC}"
+            echo -ne "${GREEN}║${WHITE}"
+            printf "%*s%s%*s" $pad1 "" "$line1" $((width - pad1 - ${#line1})) ""
+            echo -e "${GREEN}║${NC}"
+            echo -e "${GREEN}╠════════════════════════════════════════════════════════════╣${NC}"            
+            echo -ne "${GREEN}║${WHITE}"
+            printf "%*s%s%*s" $pad2 "" "$line2" $((width - pad2 - ${#line2})) ""
+            echo -e "${GREEN}║${NC}"            
+            echo -ne "${GREEN}║${WHITE}"
+            printf "%*s%s%*s" $pad3 "" "Username : root" $((width - pad2 - ${#line2})) ""
+            echo -e "\r${GREEN}║${WHITE}$(printf '%*s' $pad3 "")${WHITE}Password : ${YELLOW}$NEW_PASS${WHITE}$(printf '%*s' $((width - pad3 - ${#line3})) "")${GREEN}║${NC}"
+
+            echo -e "${GREEN}╚════════════════════════════════════════════════════════════╝${NC}"
+            echo -e "${RED}[ ! ] Simpan password dengan Aman.${NC}\n"
+            ;;
+
+
         "install_reviactyl")
             output "Memulai Instalasi Tema Reviactyl..."
             cd /var/www/pterodactyl || exit
@@ -102,7 +99,7 @@ execute_action() {
             tar -xzvf panel.tar.gz >>$LOG_PATH 2>&1
             rm panel.tar.gz
             
-            output "Membangun sistem dependensi (ini mungkin memakan waktu)..."
+            output "Membangun sistem dependensi..."
             chmod -R 755 storage/* bootstrap/cache/
             COMPOSER_ALLOW_SUPERUSER=1 composer install --no-dev --optimize-autoloader >>$LOG_PATH 2>&1
             php artisan migrate --seed --force >>$LOG_PATH 2>&1
@@ -112,27 +109,13 @@ execute_action() {
         "install_nook")
             output "Memulai Instalasi Tema Nookure..."
             cd /var/www/pterodactyl || exit
-            
-            output "Memasukkan panel ke mode maintenance..."
             php artisan down >>$LOG_PATH 2>&1
-            
-            output "Mengunduh dan mengekstrak file tema Nookure..."
             curl -L https://github.com/Nookure/NookTheme/releases/latest/download/panel.tar.gz | tar -xzv >>$LOG_PATH 2>&1
-            
-            output "Menyesuaikan perizinan file..."
             chmod -R 755 storage/* bootstrap/cache >>$LOG_PATH 2>&1
-            
-            output "Menjalankan Composer..."
             COMPOSER_ALLOW_SUPERUSER=1 composer install --no-dev --optimize-autoloader >>$LOG_PATH 2>&1
-            
-            output "Membersihkan cache sistem..."
             php artisan view:clear >>$LOG_PATH 2>&1
             php artisan config:clear >>$LOG_PATH 2>&1
-            
-            output "Menjalankan migrasi database..."
             php artisan migrate --seed --force >>$LOG_PATH 2>&1
-            
-            output "Mengembalikan panel online..."
             php artisan queue:restart >>$LOG_PATH 2>&1
             php artisan up >>$LOG_PATH 2>&1
             finalize
@@ -141,20 +124,12 @@ execute_action() {
         "uninstall")
             output "Mengembalikan Panel ke Tema Default..."
             cd /var/www/pterodactyl || exit
-            
-            output "Mengamankan konfigurasi .env..."
             cp .env /root/.env_backup >>$LOG_PATH 2>&1
-            
-            output "Menghapus tema saat ini..."
             rm -rf * >>$LOG_PATH 2>&1
             mv /root/.env_backup .env
-            
-            output "Mengunduh Panel Pterodactyl resmi..."
             curl -Lo panel.tar.gz https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz >>$LOG_PATH 2>&1
             tar -xzvf panel.tar.gz >>$LOG_PATH 2>&1
             rm panel.tar.gz
-            
-            output "Membangun ulang struktur default..."
             chmod -R 755 storage/* bootstrap/cache/
             COMPOSER_ALLOW_SUPERUSER=1 composer install --no-dev --optimize-autoloader >>$LOG_PATH 2>&1
             php artisan view:clear >>$LOG_PATH 2>&1
@@ -167,50 +142,56 @@ execute_action() {
 finalize() {
     output "Mengatur kepemilikan file (www-data)..."
     chown -R www-data:www-data /var/www/pterodactyl/* >>$LOG_PATH 2>&1
-    
-    output "Merestart layanan (Nginx & PteroQ)..."
     systemctl restart pteroq.service >>$LOG_PATH 2>&1
     systemctl restart nginx >>$LOG_PATH 2>&1
-    
-    echo -e "\n${GREEN}❖ Proses selesai dengan sukses!${NC}"
-    echo -e "${SOFT_BLUE}❖ Log instalasi dapat dilihat di: ${LOG_PATH}${NC}\n"
+    echo -e "\n${GREEN}✦ Proses selesai dengan sukses!${NC}"
+    echo -e "${SOFT_BLUE}✦ Log instalasi dapat dilihat di: ${LOG_PATH}${NC}\n"
 }
 
-# Logic Menu
 welcome
 while true; do
     options=(
+        "Reset Password VPS"
         "Install Tema Reviactyl"
         "Install Tema Nookure"
         "Uninstall Tema (Kembali ke Default)"
         "Keluar dari Installer"
     )
     actions=(
+        "reset_password"
         "install_reviactyl"
         "install_nook"
         "uninstall"
         "exit"
     )
 
-    output "Pilih menu eksekusi:"
+    output "Pilih Menu Eksekusi:"
     for i in "${!options[@]}"; do
         echo -e "  ${SOFT_BLUE}[$i]${NC} ${WHITE}${options[$i]}${NC}"
     done
     echo ""
 
-    echo -ne "${YELLOW}❖ Masukkan pilihan (0-$((${#actions[@]} - 1))): ${NC}"
+    echo -ne "${YELLOW}✦ Masukkan pilihan (0-$((${#actions[@]} - 1))): ${NC}"
     read -r action
 
-    if [ -z "$action" ] || [[ ! "0 1 2 3" =~ $action ]]; then
-        error "Pilihan tidak valid, silakan coba lagi.\n"
+    if [[ ! "$action" =~ ^[0-4]$ ]]; then
+        error "Pilihan tidak valid, silakan masukkan angka 0-4.\n"
         continue
     fi
 
-    if [ "$action" == "3" ]; then
+    if [ "$action" == "4" ]; then
         output "Keluar dari installer. Terima kasih!"
         exit 0
     fi
 
     execute_action "${actions[$action]}"
+    
+    if [ "$action" == "0" ]; then
+        echo -ne "${YELLOW}Tekan [Enter] untuk kembali ke menu...${NC}"
+        read -r
+        welcome
+        continue
+    fi
+    
     break
 done
